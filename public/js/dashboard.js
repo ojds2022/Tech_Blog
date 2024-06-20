2227fa0133d644abdf7526bc94c6cabfc9215d47
ojds2022 <odiazstrandberg@gmail.com>
2024-06-19 18:12:12 -0700

    feat: most recent posts on dashboard, updated home page layout

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -2,6 +2,11 @@ const newPostBtn = document.getElementById('newPostBtn');
 const newPostForm = document.getElementById('newPostForm');
 
 newPostBtn.addEventListener('click', () => {
+    // hides the recent posts
+    const recentPostsContainer = document.getElementById('recentPostsContainer');
+    recentPostsContainer.classList.toggle('d-none');
+
+    // reveals the form to create a new post
     const newPostForm = document.getElementById('newPostForm');
     newPostForm.classList.toggle('d-none');
 });
--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -7,8 +7,12 @@ router.get('/', async (req, res) => {
         // fetch all blog post records from the BlogPosts table in the database
         const allBlogPostData = await BlogPosts.findAll();
 
+        const dataReversed = allBlogPostData.slice().reverse(); // reverses the array of blog posts
+
+        const topFourPosts = dataReversed.slice(0,4); // takes the four most recently created posts
+
         // convert each Sequelize model instance to a plain JavaScript object
-        const blogPosts = allBlogPostData.map((blogPost) => blogPost.get({ plain: true }));
+        const blogPosts = topFourPosts.map((blogPost) => blogPost.get({ plain: true }));
 
         res.render('home', {
             title: 'Home Page',
@@ -41,8 +45,19 @@ router.post('/submit', async (req, res) => {
 // dashboard route
 router.get('/dashboard', async (req, res) => {
     try {
+        // fetch all blog post records from the BlogPosts table in the database
+        const allBlogPostData = await BlogPosts.findAll();
+
+        const dataReversed = allBlogPostData.slice().reverse(); // reverses the array of blog posts
+
+        const topEightPosts = dataReversed.slice(0,8); // takes the eight most recently created posts
+
+        // convert each Sequelize model instance to a plain JavaScript object
+        const blogPosts = topEightPosts.map((blogPost) => blogPost.get({ plain: true }));
+
         res.render('dashboard', {
-            title: 'Dashboard'
+            title: 'Dashboard',
+            blogPosts
         });
     } catch (err) {
         res.status(500).json(err);
